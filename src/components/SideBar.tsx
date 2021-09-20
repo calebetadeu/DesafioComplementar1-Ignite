25c46adfb2ca4a460f8fc05183216ea252d14032
Calebe Tadeu <calebetadeu@gmail.com>
2021-09-20 17:00:21 -0300

    completeChallenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
+import { useContext, useEffect, useState } from "react";
+import { MovieContext } from "../contexts/ApiContexts";
+import { api } from "../services/api";
+import { Button } from "./Button";
+
+
 export function SideBar() {
-  // Complete aqui
+
+const {genres,handleClickButton,selectedGenreId}=useContext(MovieContext)
+
+  
+
+return (
+
+ <>
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+  </>
+
+        
+)
 }
